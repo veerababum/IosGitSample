6c6925cb82c4a94074fed9ad6d152727e12382ec
veerababu m <veeru060786@gmail.com>
2016-08-26 18:57:37 -0600

    Initial Commit

---
+++ sample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  sample
+//
+//  Created by veerababu mulugu on 8/26/16.
+//  Copyright © 2016 veerababu m. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ sample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  sample
+//
+//  Created by veerababu mulugu on 8/26/16.
+//  Copyright © 2016 veerababu m. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
