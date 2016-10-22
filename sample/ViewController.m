fb752e27f80ae0d7f55cc0b24956662ebda027ca
ramki115 <jallaram115@gmail.com>
2016-10-22 10:48:37 -0600

    added git key for test

--- sample/ViewController.m
+++ sample/ViewController.m
@@ -16,6 +16,9 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    NSLog(@"added test key fro git");
+    //added git test key
     // Do any additional setup after loading the view, typically from a nib.
     //vee
     // testing added.....
